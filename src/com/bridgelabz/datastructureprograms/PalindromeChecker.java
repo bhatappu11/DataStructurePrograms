616d60ab65303e38ea720f94d82674f8d492786b
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-04 23:00:54 +0530

    MODIFIED: DeleteAtEnd() modified to handle even length string

--- src/com/bridgelabz/datastructureprograms/PalindromeChecker.java
+++ src/com/bridgelabz/datastructureprograms/PalindromeChecker.java
@@ -1,36 +1,32 @@
 package com.bridgelabz.datastructureprograms;
 
 import com.bridgelabz.linkedlistfiles.*;
+import java.util.*;
 
-public class PalindromeChecker {
-
+public class PalindromeChecker{
+	private static boolean isPalindrome(char[] charArray) {
+		  DequeLinkedList<Character> deque = new DequeLinkedList<Character>();
+		  for(char ch : charArray) {
+		   deque.insertRear(new Node<Character>(ch));
+		  }
+		  while(deque.size()!=0 && deque.size()!=1) {
+			  Character front=deque.removeFront();
+			  Character rear=deque.removeRear();
+			  if(front.equals(rear)) {
+				  continue;
+			  }
+			  else
+				  return false;
+		  }
+		  return true;
+		 }
 	public static void main(String[] args) {
-		String string = "racecar";
-		String removedFront = "";
-		String removedRear = "";
-		DequeLinkedList<Character> deque = new DequeLinkedList<Character>();
-		for (int i = 0; i < string.length(); i++) {
-			deque.insertRear(new Node<Character>(string.charAt(i)));
-		}
-		for (int i = 0; i < string.length(); i++) {
-			removedRear += deque.getRear().getKey();
-			deque.removeRear();
-		}
-		for (int i = 0; i < string.length(); i++) {
-			deque.insertRear(new Node<Character>(string.charAt(i)));
-		}
-		for (int i = 0; i < string.length(); i++) {
-			removedFront += deque.getFront().getKey();
-			deque.removeFront();
-		}
-		System.out.println("Rear delete : " + removedRear);
-		System.out.println("Front delete : " + removedFront);
-		if (removedRear.equals(removedFront)) {
-			System.out.println("String is palindrome");
-		} else {
-			System.out.println("String is not palindrome");
-		}
-
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a String");
+		String string = scanner.nextLine();
+		char[] charArray = string.toCharArray();
+		  
+		System.out.println("Is the String "+string+" a palindrome : "+ isPalindrome(charArray));
+		scanner.close();
 	}
-
 }
